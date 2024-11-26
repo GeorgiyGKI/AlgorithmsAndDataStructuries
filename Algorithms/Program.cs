8c259dfc0f88bf54f27256ed11de1f978744c7b9
GeorgiyGKI <georgiygki@gmail.com>
2024-11-26 17:46:37 +0300

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] charArray = { 'd', 'a', 'f', 'b', 'z', 'm', 'e', 'y' };
+			string[] stringArray = { "delta", "alpha", "foxtrot", "bravo", "zulu", "mike", "echo", "yankee" };
+
+			Console.WriteLine("Original char array:");
+			PrintArray(charArray);
+			Sorting.QuickSort(charArray);
+			Console.WriteLine("\nSorted char array:");
+			PrintArray(charArray);
+
+			Console.WriteLine("\nOriginal string array:");
+			PrintArray(stringArray);
+			Sorting.QuickSort(stringArray);
+			Console.WriteLine("\nSorted string array:");
+			PrintArray(stringArray);
+		}
+
+		static void PrintArray<T>(T[] array)
+		{
+			foreach (T item in array)
+			{
+				Console.Write(item + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Algorithms/QuickSort.cs
@@ -0,0 +1,38 @@
+﻿namespace Algorithms
+{
+	public static class Sorting
+	{
+		public static void QuickSort<T>(T[] array) where T : IComparable<T>
+		{
+			QuickSortRecursion(array, 0, array.Length - 1);
+		}
+
+		private static void QuickSortRecursion<T>(T[] array, int left, int right) where T : IComparable<T>
+		{
+			if (left < right)
+			{
+				int pivotIndex = Partition(array, left, right);
+				QuickSortRecursion(array, left, pivotIndex - 1);
+				QuickSortRecursion(array, pivotIndex + 1, right);
+			}
+		}
+
+		private static int Partition<T>(T[] array, int left, int right) where T : IComparable<T>
+		{
+			T pivot = array[right];
+			int i = left - 1;
+
+			for (int j = left; j < right; j++)
+			{
+				if (array[j].CompareTo(pivot) <= 0)
+				{
+					i++;
+					(array[i], array[j]) = (array[j], array[i]);
+				}
+			}
+
+			(array[i + 1], array[right]) = (array[right], array[i + 1]);
+			return i + 1;
+		}
+	}
+}
